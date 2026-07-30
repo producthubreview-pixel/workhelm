cf0665b5e2ef21e844d39e834325da572e7c05aa
producthubreview-pixel <producthubreview@gmail.com>
2026-07-30 12:59:17 -0400

    Update stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 
 export const stripe = process.env.STRIPE_SECRET_KEY
   ? new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: "2025-03-31",
+      apiVersion: "2024-06-20",
     })
   : null;
 
